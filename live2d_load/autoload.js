7cfacde554c4bce4bc414fd247f9024ef1a3db25
LY <1334850101@qq.com>
2021-02-09 18:01:47 +0800

    0

--- live2d_load/autoload.js
+++ live2d_load/autoload.js
@@ -1,43 +1,42 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/zxy678/live2d/live2d_load/";
-const tips_path = "https://cdn.jsdelivr.net/gh/zxy678/live2d/live2d_load/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/xiwangly2/live2d/live2d_load/";
+const tips_path = "https://cdn.jsdelivr.net/gh/xiwangly2/live2d/live2d_load/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
-	return new Promise((resolve, reject) => {
-		let tag;
+    return new Promise((resolve, reject) => {
+        let tag;
 
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-		}
-		else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
-		}
-		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
-		}
-	});
+        if (type === "css") {
+            tag = document.createElement("link");
+            tag.rel = "stylesheet";
+            tag.href = url;
+        } else if (type === "js") {
+            tag = document.createElement("script");
+            tag.src = url;
+        }
+        if (tag) {
+            tag.onload = () => resolve(url);
+            tag.onerror = () => reject(url);
+            document.head.appendChild(tag);
+        }
+    });
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
-	Promise.all([
-		loadExternalResource(live2d_path + "waifu.min.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-	]).then(() => {
-		initWidget({
-			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://misakaloli.com/api/",
-			//cdnPath: "https://cdn.jsdelivr.net/gh/zxy678/live2d/live2d_api/"
-		});
-	});
+Promise.all([
+    loadExternalResource(live2d_path + "waifu.min.css", "css"),
+    loadExternalResource(live2d_path + "live2d.min.js", "js"),
+    loadExternalResource(live2d_path + "waifu-tips.js", "js")
+]).then(() => {
+    initWidget({
+        waifuPath: tips_path + "waifu-tips.min.json",
+        apiPath: "https://xiwangly.top/api/",
+        //cdnPath: "https://cdn.jsdelivr.net/gh/xiwangly2/live2d/live2d_api/"
+    });
+});
 console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
            ＼ ', !-─‐-i  /  /´
--- live2d_load_own/autoload.js
+++ live2d_load_own/autoload.js
@@ -1,43 +1,42 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://你的域名/live2d_load_own/";
-const tips_path = "https://你的域名/live2d_load_own/";
+const live2d_path = "https://xiwangly.top/live2d_load_own/";
+const tips_path = "https://xiwangly.top/live2d_load_own/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
-	return new Promise((resolve, reject) => {
-		let tag;
+    return new Promise((resolve, reject) => {
+        let tag;
 
-		if (type === "css") {
-			tag = document.createElement("link");
-			tag.rel = "stylesheet";
-			tag.href = url;
-		}
-		else if (type === "js") {
-			tag = document.createElement("script");
-			tag.src = url;
-		}
-		if (tag) {
-			tag.onload = () => resolve(url);
-			tag.onerror = () => reject(url);
-			document.head.appendChild(tag);
-		}
-	});
+        if (type === "css") {
+            tag = document.createElement("link");
+            tag.rel = "stylesheet";
+            tag.href = url;
+        } else if (type === "js") {
+            tag = document.createElement("script");
+            tag.src = url;
+        }
+        if (tag) {
+            tag.onload = () => resolve(url);
+            tag.onerror = () => reject(url);
+            document.head.appendChild(tag);
+        }
+    });
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
-	Promise.all([
-		loadExternalResource(live2d_path + "waifu.min.css", "css"),
-		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
-	]).then(() => {
-		initWidget({
-			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://你的域名/live2d_api/",
-			//cdnPath: "https://你的CDN域名/live2d_api/"从本地加载此项不用管
-		});
-	});
+Promise.all([
+    loadExternalResource(live2d_path + "waifu.min.css", "css"),
+    loadExternalResource(live2d_path + "live2d.min.js", "js"),
+    loadExternalResource(live2d_path + "waifu-tips.js", "js")
+]).then(() => {
+    initWidget({
+        waifuPath: tips_path + "waifu-tips.min.json",
+        apiPath: "https://xiwangly.top/live2d_api/",
+        //cdnPath: "https://你的CDN域名/live2d_api/"从本地加载此项不用管
+    });
+});
 console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
            ＼ ', !-─‐-i  /  /´
